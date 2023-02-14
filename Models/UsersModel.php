14f0933ab70744806df81952a4659d5a9b21f007
Csylvainc <81378896+Csylvainc@users.noreply.github.com>
2023-02-14 11:46:49 +0100

    with usersModel ok

--- Models/UsersModel.php
+++ Models/UsersModel.php
@@ -1,3 +1,87 @@
 <?php
 
 namespace Models;
+
+use PDO;
+use App\Db;
+
+class UsersModel extends Db{
+    //////////////////// CRUD //////////////////////////////////
+
+    ///////////////// Méthode le lecture
+
+    //Trouver tous les utilisateurs
+    public static function findAll(){
+        $request = "SELECT * FROM users";
+        $response = self::getDb()->prepare($request);
+        $response->execute();
+
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Trouver un utilisateur par son id
+    /**
+     * Attend un id d'utilisateur
+     * @param array $id[int]
+     */
+    public static function findById(array $id){
+        $request = "SELECT * FROM users WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($id);
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Trouver un utilisateur par son login
+    /**
+     * Attend un login d'utilisateur
+     * @param array $login[string]
+     */
+    public static function findByLogin(array $login){
+        $request = "SELECT * FROM users WHERE login = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($login);
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Trouver un utilisateur par son login ou son id
+    /**
+     * Cette méthode permet de toruver un ou des utilisateurs par n'importe quel critère
+     * elle attend un tableau associatif
+     * @param array $user["clé" => ["valeur"]]
+     */
+    public static function findBy(array $user){
+        $request = "SELECT * FROM users WHERE " . key($user) ."= ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute(current($user));
+
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // ///////////////////// Méthode d'écriture //////////////////////////
+
+    // Créer un nouvel utilsateur
+    public static function create(array $data){
+        // $data est un tableau qui contient les valeurs à inserer en BDD
+        
+        $request = "INSERT INTO users (login, password, firstName, lastName, adress, cp,  city) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $response = self::getDb()->prepare($request);
+        $response->execute($data);
+    }
+
+    // Modification d'un utilisateur
+    public static function update(array $data){
+        $request = "UPDATE users SET login = ?, password = ?, firstName = ?, lastName = ?, adress = ?, cp = ?, city =? WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($data);
+    }
+
+    // ////////////////// Méhtode de suppression ////////////////////
+    public static function delete(array $id){
+        $request = "DELETE FROM users WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+
+        return $response->execute($id);
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Models\AnnoncesModel;
+use Models\UsersModel;
 
 require_once('autoloader.php');
 
@@ -35,9 +36,39 @@ require_once('autoloader.php');
 
 // Teste de la méthode Update()
 // $data = [2,"tondeuse à gazon","maximum 250m², moteur électrique", 185, "tondeuse.jpg", 4];
-AnnoncesModel::update($data);
+// AnnoncesModel::update($data);
 
 // Teste de la méthode delete($id)
 
 // $id = [4];
 // AnnoncesModel::delete($id);
+
+// Test de la méthode findAll() users
+// var_dump(UsersModel::findAll());
+
+// Test de la méthode findBiId() users
+// $toto = [2];
+// var_dump(UsersModel::findById([1]));
+
+// Test de la méthode findByLogin() users
+// $login = ["toto@t.fr"];
+// var_dump(UsersModel::findBylogin($login));
+
+
+// Test de la méthode findBy() users
+// $user = ['password' => ['1234']];
+// $user = ['idUser' => [1]];
+// var_dump(UsersModel::findBy($user));
+
+// Test de la méthode create() users
+$pass = password_hash("1234", PASSWORD_DEFAULT);
+// $data = ['test2@gmail.com', $pass, 'test2', 'nomTest', '66 rue de paris', 77140, 'nemours'];
+// UsersModel::create($data);
+
+
+// Test de la méthode update() users
+// $data = ['sylvain@gmail.com', $pass, 'sylvain', 'campos', '21 av des yvris', 93160, 'Noisy le grand', 2];
+// UsersModel::update($data);
+
+// Test de la méthode delete() users
+// UsersModel::delete([4]);
